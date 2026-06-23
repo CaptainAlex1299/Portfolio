972c648555e21f6f113ff4fe006cf35bc017f156
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-06-23 08:14:20 +0200

    changed some about me text

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,14 +4,14 @@ const AboutMe = () => {
   return (
     <section id="about" className="about-me">
       <div className="about-container">
-        <h2 className="">Front-End Developer | Building Interactive Web Experiences 👋</h2>
+        <h2 className="">Full-Stack Developer | Software Tester</h2>
         <p className="">
-        Hi, I'm Alexander, a web developer from South Africa. I specialize in front-end development with HTML, CSS, JavaScript, and React, and have experience with PHP and SQL for back-end work.
-        I’m passionate about creating responsive, user-friendly websites and continually improving my skills in Node.js and TypeScript.
+        Hi, I'm Alexander, a web developer from South Africa. I specialize in both front and back-end development with HTML, CSS, JavaScript, and React, PHP, SQL and have experience with and Java.
+        I enjoy building applications and I'm improving my skills in Node.js, TypeScript and Java. I've also learned software testing using tools like Playwright.
         </p>
         <p className="about-text">
-        I’m eager to work on projects that are meaningful and have a real impact.
-        I’m constantly learning new technologies and development practices to stay up-to-date and continue growing as a developer.
+        I'm eager to work on projects that are meaningful and have a real impact.
+        I'm constantly learning new technologies and development practices to stay up-to-date and continue growing as a developer.
         </p>
         <a href="#contact" className="">
           Contact Me
